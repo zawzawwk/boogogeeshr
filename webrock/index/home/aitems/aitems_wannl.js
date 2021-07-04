df978c4b9d1092ff908050d13de0f447a412923d
zawzawwk <jumi78367227806@yeah.net>
2021-07-04 09:03:41 +0800

    no commit message

--- webrock/index/home/aitems/aitems_wannl.js
+++
@@ -1,3 +0,0 @@
-homeitems.wannl = {
-	xtype: 'calendarpanel',height:350,title:'万年历',bodyStyle:(stylebody[0] != '2')?'background:white':'',bbartext:false,margin:margin,closable:false,frame: true,padding:0,border:false
-}
